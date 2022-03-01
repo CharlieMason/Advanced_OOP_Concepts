864356cc993498824441655bb4e3a410d856c6c3
Nitish K Singh <maerdcorp@outlook.com>
2022-02-28 23:18:37 -0600

    h2.1

--- GenericsAndCollections/Collections1/src/Deck.java
+++ GenericsAndCollections/Collections1/src/Deck.java
@@ -1,6 +1,7 @@
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.*;
 
 public class Deck implements Iterable<Card>{
     private List<Card> deck;
@@ -9,7 +10,23 @@ public class Deck implements Iterable<Card>{
 /* TODO 
 This constructor initializes the deck variable with
 all the cards of a deck, except a joker.
-The suits array may help you.*/        }
+The suits array may help you.*/
+        this.deck = new ArrayList<>();
+        for (String suit : suits) {
+            this.deck.add(new Card<String, String>("Ace", suit));
+            this.deck.add(new Card<Integer, String>(2, suit));
+            this.deck.add(new Card<Integer, String>(3, suit));
+            this.deck.add(new Card<Integer, String>(4, suit));
+            this.deck.add(new Card<Integer, String>(5, suit));
+            this.deck.add(new Card<Integer, String>(6, suit));
+            this.deck.add(new Card<Integer, String>(7, suit));
+            this.deck.add(new Card<Integer, String>(8, suit));
+            this.deck.add(new Card<Integer, String>(9, suit));
+            this.deck.add(new Card<Integer, String>(10, suit));
+            this.deck.add(new Card<String, String>("Jack", suit));
+            this.deck.add(new Card<String, String>("Queen", suit));
+            this.deck.add(new Card<String, String>("King", suit));
+        }
     }
 
     public Card dealCard(){
@@ -17,13 +34,18 @@ The suits array may help you.*/        }
 Code this. It is almost EXACTLY as the  method to draw one object
 from the bag in the previous task. The difference is that this method
 REMOVES the card from the deck */
+        int index = new Random().nextInt(this.deck.size());
+
+        return (this.deck.remove(index));
     }
 
     public int size(){/* TODO: Return the size of the deck (number of cards left in it)*/
+        return (this.deck.size());
     }
     @Override
     public Iterator<Card> iterator() {
        /* TODO 
 Return a new DeckIterator with this instance in the constructor.*/
+        return (new DeckIterator(this));
     }
 }
--- GenericsAndCollections/Collections2/src/Counter.java
+++ GenericsAndCollections/Collections2/src/Counter.java
@@ -1,21 +1,31 @@
 import java.util.HashMap;
+import java.util.*;
 import java.util.Set;
 
+
 public class Counter<E> {
-    private HashMap/* TODO 
-The HashMap should accomodate a generic class for the keys and Integer for the value*/ counts;
+    private HashMap/* TODO
+The HashMap should accomodate a generic class for the keys and Integer for the value*/ <E, Integer>counts;
 
     public Counter(E[] things){
+        this.counts = new HashMap<>();
 /* TODO
 Loop through the elements of "things" and use the HashMap "counts"
 to store the elements of things as keys, and the number of times they
-appear in the array as values. Check the instructions. */ }
+appear in the array as values. Check the instructions. */
+        for (int i = 0; i < things.length; i++) {
+            // get the number of times `ith` thing appeared before
+            int count = this.counts.getOrDefault(things[i], 0);
+            // increase the count in the map
+            this.counts.put(things[i], count + 1);
+        }
     }
 
     public Integer getCount(E element){
       /* TODO 
 This just returns the specific count for an element. It is a wrapper
-for a getting the value of the HashMap given the key "element" */);
+for a getting the value of the HashMap given the key "element" */
+        return (this.counts.getOrDefault(element, 0));
     }
 
     public int getSize(){
@@ -23,6 +33,20 @@ for a getting the value of the HashMap given the key "element" */);
     }
 
     public E mostFrequent()/* TODO 
-Returns the most frequent key in the HashMap*/t;
+Returns the most frequent key in the HashMap*/
+    {
+        E frequentItem = null;
+        Integer frequency = 0;
+        for (Map.Entry<E, Integer> entry : this.counts.entrySet()) {
+            E key = entry.getKey();
+            Integer value = entry.getValue();
+            if (value > frequency) {
+                frequency = value;
+                frequentItem = key;
+            }
+        }
+        return (frequentItem);
+
+
     }
 }
